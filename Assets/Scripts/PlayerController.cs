edcfe1167516788188986a510bc27e678b068d6c
Nikym <mepower2001@gmail.com>
2020-05-03 21:05:31 -0500

    done
    done so far

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject shooterPrefab;
+    float horizontalInput;
+    float verticalInput;
+    float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+       // verticalInput = Input.GetAxis("Vertical");
+
+     //   transform.Translate(Vector3.up * verticalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(shooterPrefab, transform.position, shooterPrefab.transform.rotation);
+        }
+
+    }
+}
